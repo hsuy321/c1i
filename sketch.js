57df67a46ac2f3fbbef9624ebf3e0da356d9a240
hsuy321 <hsuy321@newschool.edu>
2024-11-30 23:01:16 -0500

    fds

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var canvas;
 function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
     canvas.position(0, 0);
-    canvas.style('z-index', '-1')
+    canvas.style('z-index', '1')
 }
 
 function draw() {
